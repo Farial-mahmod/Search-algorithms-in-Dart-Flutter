02c67ff7639a8f2001534a7126352261e242b737
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2022-04-18 07:01:10 +0000

    adding the binary search program
    This program can implement Binary search in Dart language . Use "dart binary.dart" from the terminal to run .

---
+++ binary.dart
@@ -0,0 +1,33 @@
+void main() {
+print("Starting the Binary search . .");
+binarySearch();
+}
+
+binarySearch(){
+  // One may use his or her custom list or take as user-input
+  var elements = [0,1,2,3,4,5,6];
+  // el represents the element to be searched, one may use her / his own or accept as input
+  var el = 2;
+  var first = 0;
+  var last = elements.length-1;
+  var mid = ((first + last) / 2).floor();
+
+while(first<=last){
+    
+    // incrementing the middle element when the middle element is less than the element to be searched  
+    if(elements[mid] < el){
+      mid = mid + 1;
+    }
+   // decrementing the middle element when the middle element is greater than the element to be searched  
+   else if(elements[mid]>el){
+    mid = mid - 1;
+ }
+    else {
+     print("Found it: ${mid}");
+     break;
+    }
+   
+  
+  }
+ 
+}
