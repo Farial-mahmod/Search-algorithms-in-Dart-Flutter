d1db6bd00f62c1c38ff297b5301228efd1e84861
Farial Mahmod <60639522+Farial-mahmod@users.noreply.github.com>
2024-09-04 01:08:09 +0600

    Updating binary.dart with comments

--- Binary/binary.dart
+++ Binary/binary.dart
@@ -1,8 +1,10 @@
 void main() {
-print("Starting the Binary search . .");
-binarySearch();
+  print("Starting the Binary search . .");
+  // calling the binarySearch() function defined below
+  binarySearch();
 }
 
+// binarySearch() performs the binary search on a custom list
 binarySearch(){
   // One may use his or her custom list or take as user-input
   var elements = [0,1,2,3,4,5,6];
@@ -12,8 +14,8 @@ binarySearch(){
   var last = elements.length-1;
   var mid = ((first + last) / 2).floor();
 
+// loop to remain in execution as long as first is less than or equal to last
 while(first<=last){
-    
     // incrementing the middle element when the middle element is less than the element to be searched  
     if(elements[mid] < el){
       mid = mid + 1;
@@ -23,10 +25,11 @@ while(first<=last){
     mid = mid - 1;
  }
     else {
+     // displaying the found element
      print("Found it: ${mid}");
      break;
+    // breaking out of the loop at the end
     }   
   
   }
- 
 }
